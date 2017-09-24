5d3a47d7b5d6c4e870859f7bfcccf946378c53ca
Tuyen Giang <Nguyentuyengiangbn@gmail.com>
2017-09-24 21:08:57 +0700

    Demo website sysdney

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Trang Chủ</title>
+	<meta charset="utf-8"/>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<div id="top">
+			TOP
+	</div><!--too-->
+	<div id="header">
+		logo va menu & timkiem
+
+	</div><!--header-->
+	<div id="slider">
+		slider anh
+	</div><!--slider-->
+
+
+	<div id="khoa-hoc">
+		Cac khoa hoc tai sysdney
+
+	</div><!--khoa-hoc-->
+	<div id="lua-chon">
+		Lua chon cua ban
+	</div><!--lua-chon-->
+	<div id="gioi-thieu">
+		giao vien
+
+	</div><!--gioi-thieu-->
+	<div id="video">
+		video
+
+	</div><!--video-->
+
+	<div id="cam-nhan">
+		cam nhan
+
+	</div><!--cam-nhan-->
+	<div id="bottom">
+		bottom
+
+	</div><!--bottom-->
+</body>
+</html>
