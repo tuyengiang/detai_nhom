7b831708951796ca2aa013a8b1b0f87445a1d286
Tuyen Giang <Nguyentuyengiangbn@gmail.com>
2017-09-30 08:24:27 +0700

    De tai nhom - dem0

--- index.php
+++ index.php
@@ -1,46 +1,400 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Trang Chủ</title>
+	<title>Học Viện Anh Ngữ Quốc Tế</title>
 	<meta charset="utf-8"/>
+	<!--meta seo-->
+	<meta name="author" content="Tuyển giảng, quang hiếu, thế anh, hồng hải">
+	<meta name="description" content="Trang web học viện ngôn ngữ sysdney">
+	<meta name="keyword" content="sysdney,học viện sysdney, khóa học tiếng anh online, trung tâm đào tạo tiếng anh,học viên anh ngũ, khóa học tiếng anh giao tiếp">
+	<!--css-->
+	<link rel="stylesheet" type="text/css" href="js/jquery.bxslider.css">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/awesom/css/font-awesome.min.css">
+	<link rel="shortcut icon" href="img/logo.png" type="image/x-icon">
+	<link rel="icon" href="img/logo.png" type="image/x-icon"/>
+	<!--jquery-->
+	<script src="js/jquery.js"></script>
+	<script src="js/jquery.bxslider.min.js"></script>
+	<script src="js/main.js"></script>
+	<script lang="javascript">(function() {var pname = ( (document.title !='')? document.title : document.querySelector('h1').innerHTML );var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async=1; ga.src = '//live.vnpgroup.net/js/web_client_box.php?hash=a40d11a8d0ee5c046117cf29d5359458&data=eyJzc29faWQiOjk3NDI2NCwiaGFzaCI6IjgxMGQ5YzdiNTBmN2RlOWM2YzhkYWUzYzYxZTJiYmU0In0-&pname='+pname;var s = document.getElementsByTagName('script');s[0].parentNode.insertBefore(ga, s[0]);})();</script>	
+
 </head>
 <body>
+	<a href="#top"></a>
 	<div id="top">
-			TOP
+	    <div id="top-content">
+	    	<div class="top-left">
+		    	<a href="#" ><i class="fa fa-map-marker"></i> Số 126 Lê Hồng Phong , Nam Định </a>| 	
+			<a href="#" <i class="fa fa-envelope-square"></i> Anhngusysdney@gmail.com </a>| 
+			<a href="#" <i class="fa fa-bell"></i> 8H00 - 20H00 </a>| 
+			<a href="#" <i class="fa fa-phone"></i> 0934555000</a>
+		</div><!--top-left-->
+		<div class="top-right">
+			<a href="#" title="facebook"><i class="fa fa-facebook"></i> </a>  
+			<a href="#" title="Twitter"><i class="fa fa-twitter"></i> </a> 
+			<a href="#" title="google"><i class="fa fa-google"></i> </a> 
+			<a href="#" title="youtube"><i class="fa fa-youtube"></i> </a>  
+		</div><!--top-right-->
+	   </div><!--top-content-->
 	</div><!--too-->
 	<div id="header">
-		logo va menu & timkiem
+	     <div id="header-content">
+	     	<div class="logo">
+			<a href="index.php" title="Trang Chủ"><img src="img/logo.png" title="Trang Chủ Sysdney"></a>
+		</div><!--logo-->
+		<div class="menu">
+			<ul>
+				<li><a href="index.php"><i class="fa fa-home"></i> TRANG CHỦ</a></li>
+				<li><a href="#">GIỚI THIỆU</a></li>
+				<li><a href="#">KHÓA HỌC</a></li>
+				<li><a href="#">
+TIN TỨC</a></li>
+				<li><a href="#">TUYỂN DỤNG</a></li>
+				<li><a href="#">LIÊN HỆ</a></li>
 
+			</ul>
+		</div><!--menu-->
+		<div class="search">
+			<button id="btn" title="Tìm Kiếm"><i class="fa fa-search"></i></button>
+			<div class="search-btn">
+				<form method="post" action="search.php">
+					<input type="search" name="search" placeholder="Bạn Tìm Gì Nào">
+				</form>	
+
+			</div><!--search-btn-->
+		</div><!--search-->
+
+	     </div><!--header-content-->
 	</div><!--header-->
+
+	<!--menu scroll-->
+	<div id="header-scroll">
+	     <div id="header-content-scroll">
+	     	<div class="logo">
+			<a href="index.php" title="Trang Chủ"><img src="img/logo.png" title="Trang Chủ Sysdney"></a>
+		</div><!--logo-->
+		<div class="menu">
+			<ul>
+				<li><a href="index.php"><i class="fa fa-home"></i> TRANG CHỦ</a></li>
+				<li><a href="#">GIỚI THIỆU</a></li>
+				<li><a href="#">KHÓA HỌC</a></li>
+				<li><a href="#">
+TIN TỨC</a></li>
+				<li><a href="#">TUYỂN DỤNG</a></li>
+				<li><a href="#">LIÊN HỆ</a></li>
+
+			</ul>
+		</div><!--menu-->
+		<div class="search">
+			<button id="btn" title="Tìm Kiếm"><i class="fa fa-search"></i></button>
+			<div class="search-btn">
+				<form method="post" action="search.php">
+					<input type="search" name="search" placeholder="Bạn Tìm Gì Nào">
+				</form>	
+
+			</div><!--search-btn-->
+		</div><!--search-->
+
+	     </div><!--header-content-->
+	</div><!--header-->
+	
+
 	<div id="slider">
-		slider anh
+			
+		<ul class="bixslider">
+			<li><a href="#"><img src="img/1.jpg"></a></li>
+			<li><a href="#"><img src="img/2.jpg"></a></li>
+			<li><a href="#"><img src="img/3.jpg"></a></li>
+			<li><a href="#"><img src="img/4.jpg"></a></li>
+			<li><a href="#"><img src="img/5.jpg"></a></li>
+		</ul>
+
 	</div><!--slider-->
 
-
 	<div id="khoa-hoc">
-		Cac khoa hoc tai sysdney
+		<h3 >
+			CÁC KHÓA HỌC TẠI ANH NGỮ SYDNEY	
+			<hr>
+		</h3>
+		<br>
+		<div class="khoa-hoc-content">
+			<div class="khoa-hoc-image">
+				<img src="img/tienganhtreem.jpg">
+			</div>
+			<div class="clear"></div>
+			<div class="khoa-hoc-slider">
+				<h4>
+					Tiếng Anh trẻ em
+				</h4>
+				<p>
+					Dành cho các bé từ 3-6 tuổi.
+				</p>
+				<a href="" class="khoa-hoc-button">
+					<button type="button">
+						<span>CHI TIẾT ></span>
+					</button>
+				</a>
+			</div>
+		</div> <!-- khoa-hoc-content -->
+		<div class="khoa-hoc-content">
+			<div class="khoa-hoc-image">
+				<img src="img/tienganhmamnon.jpg">
+			</div>
+			<div class="khoa-hoc-slider">
+				<h4>
+					Tiếng Anh Mầm non
+				</h4>
+				<p>
+					Dành cho các bé từ lớp 1 -> lớp 5.
+				</p>
+				<a href="" class="khoa-hoc-button">
+					<button type="button">
+						<span>CHI TIẾT ></span>
+					</button>
+				</a>
+			</div>
+		</div><!-- khoa-hoc-content -->
+		<div class="khoa-hoc-content">
+			<div class="khoa-hoc-image">
+				<img src="img/tienganhthcs.jpg">
+			</div>
+			<div class="khoa-hoc-slider">
+				<h4>
+					Tiếng Anh THCS
+				</h4>
+				<p>
+					Dành cho các bé từ lớp 6 -> lớp 9.
+				</p>
+				<a href="" class="khoa-hoc-button">
+					<button type="button">
+						<span>CHI TIẾT ></span>
+					</button>
+				</a>
+			</div>
+		</div><!-- khoa-hoc-content -->
+		<div class="khoa-hoc-content">
+			<div class="khoa-hoc-image">
+				<img src="img/tienganhgiaotiep.jpg">
+			</div>
+			<div class="khoa-hoc-slider">
+				<h4>
+					Tiếng Anh Giao tiếp tổng quát
+				</h4>
+				<p>
+					Dành cho mọi đối tượng.
+				</p>
+				<a href="" class="khoa-hoc-button">
+					<button type="button">
+						<span>CHI TIẾT ></span>
+					</button>
+				</a>
+			</div>
+		</div><!-- khoa-hoc-content -->
 
 	</div><!--khoa-hoc-->
 	<div id="lua-chon">
-		Lua chon cua ban
+		<h3>
+			6 LÝ DO ĐỂ CHỌN Anh ngữ SYDNEY
+			<hr>
+		</h3>
+		<br>
+		<div class="lua-chon-content">
+			<div class="lua-chon-content-1">
+				<div class="title"><i class="fa fa-chevron-down" id="down"></i>
+					GIỜ HỌC KHỎE
+				</div>
+				<div class="lua-chon-content-1-content">
+					<p>
+						Phương pháp TIẾNG ANH HIỆN ĐẠI được áp dụng trong lộ trình giảng dạy tại SYDNEY. Kiến thức bài giảng được biên soạn thành những trò chơi vận động giúp các con tiếp thu kiến thức một cách tự nhiên và năng động. Đồng thời mỗi buổi học luôn có Tea-break giữa giờ, nhằm bổ sung năng lượng cho các con.
+					</p>	
+					<div class="lua-chon-content-1-image">
+						<img src="img/05.png">
+					</div>
+				</div>
+			</div><!-- lua-chon-content-1 -->
+			<div class="lua-chon-content-1">
+				<div class="title"><i class="fa fa-chevron-down" id="down"></i>
+					Title 2
+				</div>
+				<div class="lua-chon-content-1-content">
+					
+				</div>
+			</div><!-- lua-chon-content-1 -->
+			<div class="lua-chon-content-1">
+				<div class="title"><i class="fa fa-chevron-down" id="down"></i>Title 3
+				</div>
+				<div class="lua-chon-content-1-content">
+					
+				</div>
+			</div><!-- lua-chon-content-1 -->
+		</div><!-- lua-chon-content -->
+		<div class="lua-chon-slider">
+			<img src="img/anh1.jpg">
+		</div><!-- lua-chon-slider -->
+		<div class="lua-chon-content">
+			<div class="lua-chon-content-1">
+				<div class="title"><i class="fa fa-chevron-down" id="downup"></i> 
+					HOẠT ĐỘNG NGOẠI KHÓA
+				</div>
+				<div class="lua-chon-content-2-content">
+					<p>
+
+						Xây dựng chương trình các hoạt động ngoại khóa theo từng khóa học. Giúp các con có môi trường giao lưu, hoạt động nhóm, khám phá khơi gợi sự tò mò năng động, kích thích tư duy phát triển tự nhiên. Các hoạt động bao gồm dã ngoại, trại hè, party,…
+					</p>
+					<div class="lua-chon-content-1-image">
+						<img src="img/07.png">
+					</div>
+				</div>
+			</div><!-- lua-chon-content-1 -->
+			<div class="lua-chon-content-1">
+				<div class="title"><i class="fa fa-chevron-down" id="down5"></i> 
+					Title 2
+				</div>
+				<div class="lua-chon-content-2-content">
+					
+				</div>
+			</div><!-- lua-chon-content-1 -->
+			<div class="lua-chon-content-1">
+				<div class="title"><i class="fa fa-chevron-down" id="down"></i> 
+					Title 3
+				</div>
+				<div class="lua-chon-content-2-content">
+					
+				</div>
+			</div><!-- lua-chon-content-1 -->
+		</div><!-- lua-chon-content -->
+		<div style="clear:left;"></div>
 	</div><!--lua-chon-->
+	<div style="clear:left;"></div>
 	<div id="gioi-thieu">
-		giao vien
+	    <div class="gioi-thieu">
+	    	<div style="clear:left;"></div>
+		<div id="content">
+			<h3 style="color:white;margin-top:50px;">
+				ĐỘI NGŨ GIẢNG VIÊN CỦA SYDNEY
+				<hr>
 
-	</div><!--gioi-thieu-->
+			</h3><!--title-->
+			<div class="content-nd">
+				<div class="content-img">
+					<img src="img/a.jpg">
+				</div><!--content-img-->
+				<div class="content-title">
+					<h5>Ms. Trần Lợi</h5><br>
+					Cô Trần Thị Lợi, 5 năm kinh nghiệm hướng dẫn và giảng dạy Tiếng Anh cho trẻ em.
+				</div><!--content-title-->
+
+			</div><!--content-nd-->
+			<div class="content-nd">
+				<div class="content-img">
+					<img src="img/b.jpg">
+				</div><!--content-img-->
+				<div class="content-title">
+					<h5>Ms. Kim Thúy</h5><br>
+					Cô Trần Kim Thúy, 5 năm kinh nghiệm hướng dẫn và giảng dạy Tiếng Anh giao tiếp
+				</div><!--content-title-->
+
+			</div><!--content-nd-->
+			<div class="content-nd">
+				<div class="content-img">
+					<img src="img/c.jpg">
+				</div><!--content-img-->
+				<div class="content-title">
+					<h5>Ms. Mỹ Hương</h5><br>
+					Cô Mỹ Hương, 9 năm kinh nghiệm hướng dẫn và giảng dạy Tiếng Anh tổng quát
+				</div><!--content-title-->
+
+			</div><!--content-nd-->
+
+
+		</div><!--content-->
+	    </div><!--class-gioi-thieu-->
+		
+	</div><!--gioithieu-->
+	
 	<div id="video">
-		video
-
+		<h3>
+			VIDEO VÀ TIN TỨC SYDNEY<hr>
+		</h3>
+		<div class="main-video">
+			<iframe  src="https://www.youtube.com/embed/gFvRq6GVUdY?feature=oembed" ></iframe>
+		</div>
+		<div class="right-video">
+			<iframe  src="https://www.youtube.com/embed/gFvRq6GVUdY?feature=oembed" ></iframe>
+		</div>
+		<div class="right-video">
+			<iframe  src="https://www.youtube.com/embed/gFvRq6GVUdY?feature=oembed" ></iframe>
+		</div>	
+		<div style="clear:left;"></div>
+		<div  class="bot-video" >
+			<img   alt="Anh ngữ SYDNEY - Nơi đào tạo Tiếng Anh uy tín nhất Nam Định" src="https://img.youtube.com/vi/SL0oHvhll88/0.jpg">	</div>
+		<div  class="bot-video" >	
+			<img    alt="Anh ngữ SYDNEY - Nơi đào tạo Tiếng Anh uy tín nhất Nam Định" src="https://img.youtube.com/vi/SL0oHvhll88/0.jpg" >		</div>
+		<div  class="bot-video" >	
+			<img    alt="Anh ngữ SYDNEY - Nơi đào tạo Tiếng Anh uy tín nhất Nam Định" src="https://img.youtube.com/vi/SL0oHvhll88/0.jpg" >		</div>
+		<div  class="bot-video" >	
+			<img    alt="Anh ngữ SYDNEY - Nơi đào tạo Tiếng Anh uy tín nhất Nam Định" src="https://img.youtube.com/vi/SL0oHvhll88/0.jpg"">	</div>
+	 
+	</div>
 	</div><!--video-->
 
 	<div id="cam-nhan">
-		cam nhan
+		<h3>Cảm nhận của học viên<hr></h3>
+		<div class="comment">
+			<div class="comment-img">
+				<img   src="http://www.anhngusydney.com/wp-content/uploads/2017/05/2_DoTuanTai-280x280.jpg"  alt="">
+			</div>
+			<div class="comment-title">“Cô giáo và thầy tây dạy vui lắm ạ. Trung tâm thì đẹp ạ. Con thích được hát các bài hát tiếng anh mà cô dạy.”</div>
+		</div>
+		<div class="comment">
+			<div class="comment-img">
+				<img  src="http://www.anhngusydney.com/wp-content/uploads/2017/05/13_TranDiepChi-280x280.jpg" class="rt-responsive-img" alt="">
+			</div>
+			<div class="comment-title">“Cô giáo và thầy tây dạy vui lắm ạ. Trung tâm thì đẹp ạ. Con thích được hát các bài hát tiếng anh mà cô dạy.”</div> 
+		</div>
+		<div class="comment">
+			<div class="comment-img">
+				<img  src="http://www.anhngusydney.com/wp-content/uploads/2017/05/12_DinhGiaPhu2-280x280.jpg" class="rt-responsive-img" alt="">
+			</div>
+			<div class="comment-title">“Cô giáo và thầy tây dạy vui lắm ạ. Trung tâm thì đẹp ạ. Con thích được hát các bài hát tiếng anh mà cô dạy.”</div>
+		</div>
+	
 
 	</div><!--cam-nhan-->
+	<div style="clear:left;"></div>
 	<div id="bottom">
-		bottom
+
+		<div class="bottom-title">
+			<ul>
+				<li><a href="#">Trang Chủ</a></li>
+				<li><a href="#">Giới Thiệu</a></li>
+				<li><a href="#">Khóa Học</a></li>
+				<li><a href="#">Tin Tức</a></li>
+				<li><a href="#">Tuyển Dụng</a></li>
+				<li><a href="#">Liên Hệ</a></li>
+
+
+			</ul>
+		</div><!--bottom-title-->
+		<div class="bottom-content">
+		Coppyright 2017 &copy; Tuyển Giảng - Quang Hiếu - Hồng Hải - Thế anh
+		</div><!--bottom-content-->
 
 	</div><!--bottom-->
+
+	<div id="updown">
+		<div class="updown-header">
+				<a href="#top"><i class="fa fa-chevron-up" id="down6"></i></a>
+
+		</div><!--updown-header-->
+		<div class="updown-mess">
+				<i class="fa fa-envelope-o "></i>
+		</div><!--updown-header-->
+		<div class="updown-phone">
+				<i class="fa fa-phone"></i>
+		</div><!--updown-header-->
+
+	</div><!--updown-->
 </body>
 </html>
---
+++ js/main.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+	$(".bixslider").bxSlider();
+
+	$("#btn").on("click",function(){
+		$(".search-btn").fadeToggle(200);
+	});
+	
+
+	
+});
+
+
+$(document).ready(function(){
+	$(window).scroll(function(){
+		if($(this).scrollTop()>300){
+			$("#updown").fadeIn(200);
+		}else{
+			$("#updown").fadeOut(200);
+		}
+	});
+	$(".updown-header").on("click",function(){
+		$("html,body").animate({scrollTop:0},600);
+		return false; 
+	});
+});
+
+
+$(document).ready(function(){
+	$("#down").on("click",function(){
+		$(".lua-chon-content-1-content").toggle(400);
+		$(".title").css("background","#c4161c").css("color","white");
+	});
+
+});
+$(document).ready(function(){
+	$("#downup").on("click",function(){
+		$(".lua-chon-content-2-content").toggle(400);
+		$(".title").css("background","#c4161c").css("color","white");
+	});
+
+});
+
+$(document).ready(function(){
+	$(window).scroll(function(){
+		if($(this).scrollTop()>200){
+			$("#header-scroll").fadeIn(200);
+			
+		}else{
+			$("#header-scroll").fadeOut(200);
+		}
+	})
+})
+
+
+
